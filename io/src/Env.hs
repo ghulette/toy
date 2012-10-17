a0a056fd0fd56b5f2095c8d0a7ad696371d70587
Geoff Hulette <ghulette@gmail.com>
2012-10-17 12:48:05 -0700

    Update

--- io/src/Env.hs
+++ io/src/Env.hs
@@ -10,14 +10,14 @@ type Env = Id -> Maybe Int
 
 -- Empty environment (no variables assigned)
 empty :: Env
-empty = const Nothing
+empty = undefined
 
 -- "Update" an environment with a new binding.
 extend :: Env -> Id -> Int -> Env
-extend env k n = \x -> if x == k then Just n else env x
+extend = undefined
 
 -- Given an environment and a variable name, return the value that is assigned
 -- to that name in the environment. If the variable name is unbound, return
 -- Nothing.
 lookup :: Id -> Env -> Maybe Int
-lookup k env = env k
+lookup = undefined
--- io/src/Expr.hs
+++ io/src/Expr.hs
@@ -30,34 +30,14 @@ type Prog = [Stmt]
 -- Given an environment and an expression, evaluate the expression (i.e., 
 -- reduce the expression to an integer value).
 evalExpr :: Env -> Expr -> Int
-evalExpr env (Var x) = 
-  case lookup x env of
-    Just n -> n
-    Nothing -> 0
-evalExpr _   (Num n) = n
-evalExpr env (Op op e1 e2) = (evalOp op) (evalExpr env e1) (evalExpr env e2)
+evalExpr = undefined -- You can just copy this from the last exercise.
 
 -- Given an environment and a statement, return a new environment that extends 
 -- the old one by performing the assignment described by the statement.
 evalStmt :: Env -> Stmt -> IO Env
-evalStmt env (Assign x e) = do
-  let n    = evalExpr env e
-  let env' = extend env x n
-  putStrLn $ x ++ " = " ++ (show n)
-  return env'
-evalStmt env (Print e) = do
-  let n = evalExpr env e
-  print n
-  return env
+evalStmt = undefined
 
--- Given a sequence of assignment statements, evaluate them in left-to-right 
--- order, updating the environment as you go. Return the final environment.
-evalProg' :: Env -> Prog -> IO ()
-evalProg' _ [] = return ()
-evalProg' env (s:ss) = do
-  env' <- evalStmt env s
-  evalProg' env' ss
-
--- You can implement as foldM_ evalStmt empty but, for fun...
+-- Given a sequence of statements, evaluate them in left-to-right order. We
+-- don't care about the final Env, we just want the side effects.
 evalProg :: Prog -> IO ()
-evalProg = evalProg' empty
+evalProg = undefined
